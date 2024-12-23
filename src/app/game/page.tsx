e943048a837d9d8ed5e73942c3e93904cb3d70b5
BomScoob12 <sorawitkraukham@gmail.com>
2024-12-23 12:19:59 +0700

    refactor: Remove unused TableComponent from Game page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,8 +1,5 @@
-import TableComponent from "./table";
-
 export default function Game() {
     return <div>
         <div>This is game page</div>
-        <TableComponent tableName="BomScope"/>
     </div>
 }
